3bbfa7d8096916cba4e3117c2bb5c1ca7b8c1938
Amour BENLAOUBI <97226366+AmourBenlaoubi@users.noreply.github.com>
2022-05-21 01:01:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+    const scrollTop =
+        window.pageTOffset || this.document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = '-50px';
+    } else {
+        navbar.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});
+
+// TYPED
+var typed = new Typed('.typed', {
+    strings: [
+        'Bonjour à toutes et à tous',
+        "Après avoir suivi une carrière m'ayant fait découvrir plusieurs milieux professionnels et exercer plusieurs métiers différents. J'ai décidé de me lancer à fond dans l'apprentissage du code. Depuis plusieurs mois je me forme en autodidacte aux langages Front tels que CSS, HTML, Javascript et son Framework React, j'ai également suivi une formation de 3 mois dans le bootcamp 'La Capsule' afin de devenir le plus rapidement possible développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder.",
+    ],
+    typeSpeed: 20,
+});
+
+//Compteur
+let compteur = 0;
+
+$(window).scroll(function () {
+    const top = $('.counter').offset().top - window.innerHeight;
+
+    if (compteur == 0 && $(window).scrollTop() > top) {
+        $('.counter-value').each(function () {
+            let $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text(),
+            }).animate(
+                {
+                    countNum: countTo,
+                },
+                {
+                    duration: 10000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    },
+                },
+            );
+        });
+        compteur = 1;
+    }
+});
+
+//AOS
+AOS.init();
